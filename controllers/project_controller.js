05a610b97dd1a04567dfd88492b50a2acd54f955
Clementine <clementine.oldfield@gmail.com>
2019-07-18 22:00:15 +1000

    Created findCurrent method for projects
    To find all projects for the current user.

--- controllers/project_controller.js
+++ controllers/project_controller.js
@@ -35,7 +35,7 @@ async function update(req, res) {
   const {updatedProject} =  req.body;
   try {
     const project = await ProjectModel.updateOne(updatedProject);
-    response = "Success! Project updated.";
+    response = project;
   }catch(error){
     console.log(error);
     response =  (error);
@@ -48,7 +48,6 @@ async function update(req, res) {
 async function create(req, res) {
   let response = genericError();
   const {newProject} = req.body;
-
 
   try {
     const project = await ProjectModel.create(newProject)
@@ -65,6 +64,18 @@ async function create(req, res) {
   res.send(response);
 }
 
+function findCurrent(req, res) {
+  const { user } = req;
+  const projects = user.projects.map(el => {
+    return el.projectId;
+  });
+  ProjectModel.find({ _id: {
+    $in: projects
+  }}, function(err, docs){
+    res.send(docs);
+  });
+}
+
 function genericError(error){
   return error ? 
     `Error: While trying to ${genericError.caller} on Project-controller: " + ${error}` : 
@@ -75,5 +86,6 @@ module.exports = {
   index,
   show,
   update,
-  create
+  create,
+  findCurrent
 }
--- routes/project_routes.js
+++ routes/project_routes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
 
 const ProjectController = require("./../controllers/project_controller");
 
 router.get("/", ProjectController.index);
 router.get("/:id", ProjectController.show);
+router.get("/user/current", passport.authenticate("jwt", { session: false }), ProjectController.findCurrent);
 
 router.put("/:id/update", ProjectController.update);
 router.patch("/:id/update", ProjectController.update);
