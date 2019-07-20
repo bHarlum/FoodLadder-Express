7559264d705a532c6a25408dfc04125a881caf62
Bryce Harlum <bryce.harlum@gmail.com>
2019-07-20 12:11:07 +1000

    Removed commented code
    Removed commented code

--- app.js
+++ app.js
@@ -22,16 +22,6 @@ app.use(cors({
   }
 }));
 
-// var whitelist = ['http://example1.com', 'http://example2.com']
-// var corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('Not allowed by CORS'))
-//     }
-//   }
-// }
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
