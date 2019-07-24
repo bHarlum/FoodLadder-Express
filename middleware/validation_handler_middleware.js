5447650ae01b0bf90937f0c6b088143d1c1ccaf4
Clementine <clementine.oldfield@gmail.com>
2019-07-24 16:01:42 +1000

    Fixed custom error messages for Joi

--- middleware/validation_handler_middleware.js
+++ middleware/validation_handler_middleware.js
@@ -2,10 +2,16 @@ const { isCelebrate } = require("celebrate");
 
 module.exports = function(err, req, res, next) {
   if(isCelebrate(err)){
-    console.log(err);
     err.statusCode = 400;
-    err.message = "There seem to be errors in the form."
-    console.log("running valiation middleware");
+    console.log(err.message);
+    console.log("running validation middleware");
+    switch(err.message){
+      case "That is not a valid phone number.":
+        err.message = err.message;
+        break;
+      default:
+        err.message = "There seem to be errors in the form.";
+    }
     console.log(err.message);
     return res.status(err.statusCode).send(err.message);
   }
